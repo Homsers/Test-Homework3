e8a2a9e61e5f86267056f6219754aa867e5b6043
Maxim <you@example.com>
2022-08-17 17:07:09 +0300

    00branch

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,6 +1,1 @@
-﻿Console.WriteLine("Enter a number");
-int number0 = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter a second number");
-int number1 = int.Parse(Console.ReadLine());
-
-Console.Write(Math.Pow(number0,number1));
+﻿
