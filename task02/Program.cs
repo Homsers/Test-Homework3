d8b0b718c0579ab1a17f314f35f752ae4fbc6c69
Maxim <you@example.com>
2022-08-17 18:10:29 +0300

    comm2

---
+++ task02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter a first number");
+int number0 = int.Parse(Console.ReadLine());
+int number1 = 0;
+
+
+while (number0 > 0)
+{
+int number2 = number0 % 10;
+number0 = number0 / 10;
+number1 = number1 + number2;
+}
+
+
+Console.WriteLine(number1);
