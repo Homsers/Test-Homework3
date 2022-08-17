7b170a429e4ccc5107b35b43e019bf7ed1fec70c
Maxim <you@example.com>
2022-08-17 19:39:24 +0300

    master33

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,11 +1,17 @@
-﻿
+﻿Console.WriteLine("Enter a number of array");
+int number0 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a two random numbers");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
 void RandArray()
 {
-    int[] arr = new int [8];
+    int[] arr = new int [number0];
 
     for (int i=0; i<arr.Length; i++)
     {
-        arr[i] = new Random{}.Next(0,20);
+        arr[i] = new Random{}.Next(number1,number2);
     }
 
     Console.WriteLine($"[{String.Join(", ", arr)}]");
