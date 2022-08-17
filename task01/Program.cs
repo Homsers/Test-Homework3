24fd68e3e55fcd68ad7418897f78ac8cde761f41
Maxim <you@example.com>
2022-08-17 17:31:46 +0300

    1comm

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Enter a first number");
+int number0 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a second number");
+int number1 = int.Parse(Console.ReadLine());
+
+int A = number0;
+int B = number1;
+
+for (int i = 1; i < number1; i++)
+{
+number0 = number0 * A;
+}
+Console.WriteLine(number0);
